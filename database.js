a2bd71bab1fe57ad46ebf1674798f7b0d8f9b7c9
Huntertheheavy <153214511+Huntertheheavy@users.noreply.github.com>
2024-06-11 02:40:55 +0100

    final5

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-    host: "ode.h.filess.io",
+    host: "p3a.h.filess.io",
     user: "TimelineTyrants2_liquiddeep",
-    password: "TimelineTyrants2_liquiddeep",
-    database: "TimelineTyrants2_liquiddeep", // Schema name 
+    password: "yourmom",
+    database: "timelinetyrantsfinal_castlebet", // Schema name 
     port: "3307"
 });
 
